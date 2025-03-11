a1f7b031a21a06bfde3350c4b6f2a931ea1c804a
GoNEAR <cagatay.morgul@outlook.com>
2025-03-11 09:57:15 +0100

    ajout de changement du nombre de questions

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,15 @@ while (true) {
     console.log("1.Lancer une partie")
     console.log("2.Changer le nombre de question")
     console.log("3.Quitter")
-    option = prompt()
+    let option = prompt()
     if (option === "1") {
         
     } else if (option === "2") {
-        
+        do {
+            console.log("Entrer un nombre entre 2 et 20")
+            nombreQuestion = prompt()
+        } while (nombreQuestion > 20 || nombreQuestion < 2)
+        console.log("Le nombre de question a bien été changé en", nombreQuestion, "questions")
     } else if (option === "3") {
         break
     } else {
