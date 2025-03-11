f41044da209f8a52b65946f3e1a3bed53612c975
GoNEAR <cagatay.morgul@outlook.com>
2025-03-11 09:37:58 +0100

    ajout de 3 options vide: quitter lancer changer

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+console.log("Bienvenu dans le quizz sur les animes")
+while (true) {
+    console.log("1.Lancer une partie")
+    console.log("2.Changer le nombre de question")
+    console.log("3.Quitter")
+    option = prompt()
+    if (option === "1") {
+        
+    } else if (option === "2") {
+        
+    } else if (option === "3") {
+        break
+    } else {
+
+    }
+}
